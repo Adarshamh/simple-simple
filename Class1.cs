ea71be0ffcf84d7c67e2ca3a2cd6e1e6c29e6fa1
Adarshamh <82590411+Adarshamh@users.noreply.github.com>
2022-08-13 20:38:42 +0530

    Add project files.

---
+++ Class1.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.AccessControl;
+using System.Text;
+using System.Threading.Tasks;
+using System.Transactions;
+
+namespace sample1
+{
+     class Class1
+    {
+        public void printOut(string name)
+        {
+            Console.WriteLine(name);
+        }
+
+        public void printOut(int age)
+        {
+            Console.WriteLine(age);
+        }
+
+        public void printOut()
+        {
+            Console.WriteLine("Adarshmh");
+        }
+    }
+
+}
---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Diagnostics.CodeAnalysis;
+using sample1;
+
+
+namespace externalMethods
+{
+    public class class1
+    {
+        static void Main(string[] args)
+        {
+            //Instantiated class1 and named it as constructorClass
+            Class1 constructorClass = new Class1();
+
+            //here we call method printOut() 
+            constructorClass.printOut("Adarsh");
+            constructorClass.printOut(26);
+            constructorClass.printOut();
+        }
+      
+    }
+}
